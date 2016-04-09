8fa695fcd816915a9e29a63d885bb749a33cbc74
Matthieu <matthieubessol@gmail.com>
2016-04-09 16:59:43 +0200

    prout

--- include/Checkpoint.h
+++ include/Checkpoint.h
@@ -4,6 +4,7 @@
 #include "Point2D.h"
 #include "bool.h"
 #include "Geometry.h"
+#include <stdlib.h>
 
 typedef	struct {
 	Cercle* cercle;
@@ -12,5 +13,6 @@ typedef	struct {
 void MakeCheckpoint(Point2D pos, float r, Checkpoint* checkpt);
 bool IsCheckpoint(Point2D pos);
 void FreeCheckpoint(Checkpoint* checkpt);
+Checkpoint* CopyCheckpt(Checkpoint* checkpt);
 
 #endif
--- src/Checkpoint.c
+++ src/Checkpoint.c
@@ -1,10 +1,24 @@
 #include "Checkpoint.h"
 
+
 void MakeCheckpoint(Point2D pos, float r, Checkpoint* checkpt){
 	checkpt->cercle = (Cercle *) malloc(sizeof(Cercle));
 	MakeCercle(checkpt->cercle, pos, r);
 }
 bool IsCheckpoint(Point2D pos);
+
 void FreeCheckpoint(Checkpoint* checkpt){
-	
+	free(checkpt->cercle);
+	checkpt->cercle = NULL;
+}
+
+Checkpoint* CopyCheckpt(Checkpoint* checkpt){
+	Checkpoint *chpt = NULL;
+	if(checkpt != NULL){
+		chpt = (Checkpoint*) malloc(sizeof(Checkpoint));
+		if(chpt != NULL){
+			MakeCheckpoint(checkpt->cercle->centre, checkpt->cercle->radius, chpt);
+		}
+	}
+	return chpt;
 }
