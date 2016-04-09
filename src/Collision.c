ca9de1acea7e4058189860c821a6e4101ba790be
Matthieu <matthieubessol@gmail.com>
2016-04-09 16:58:16 +0200

    Test les collisions maispas trop

--- src/Collision.c
+++ src/Collision.c
@@ -17,30 +17,50 @@
 /*fonction qui calcule les consequences des colisions*/
 
 void CollisionVehiculeVehicule(Vehicule* vehicule1, Vehicule* vehicule2){
-	if(TouchedVehiculeVehicule(vehicule1, vehicule2) == true){
-		//printf("COLLISION VEHICULES\n");
+    if(TouchedVehiculeVehicule(vehicule1, vehicule2) == true){
+        //printf("COLLISION VEHICULES\n");
         //vehicule1-> direction = MultVector(vehicule1->direction, -1);
         //vehicule2-> direction = -MultVector(vehicule2-> direction, 10);
 
-        Vector2D newVector = Vector(vehicule1->cercle->centre, vehicule2->cercle->centre);
-        float dp = Norm(newVector);
-        newVector.x = newVector.x/dp; //idem à Normalize(newVecotor);
-        newVector.y = newVector.y/dp;
+//        Vector2D newVector = Vector(vehicule1->cercle->centre, vehicule2->cercle->centre);
+//        float dp = Norm(newVector);
+//        newVector.x = newVector.x/dp; //idem à Normalize(newVecotor);
+//        newVector.y = newVector.y/dp;
 
-       Vector2D compVitesse = SubVectors(vehicule1->vitesse, vehicule2->vitesse);
+//       Vector2D compVitesse = SubVectors(vehicule1->vitesse, vehicule2->vitesse);
 //        double dvx = vehicule1->vitesse.x - vehicule2->vitesse.x;
 //        double dvy = vehicule1->vitesse.y - vehicule2->vitesse.y;
-        double scalaire = DotProduct(newVector, compVitesse);
-        compVitesse.x = newVector.x*scalaire;
-        compVitesse.y = newVector.y*scalaire;
+//        double scalaire = DotProduct(newVector, compVitesse);
+//        compVitesse.x = newVector.x*scalaire;
+//        compVitesse.y = newVector.y*scalaire;
 
-        vehicule1->vitesse = SubVectors(vehicule1->vitesse, compVitesse);
-        vehicule2->vitesse = AddVectors(vehicule2->vitesse, compVitesse);
-	}
+//        vehicule1->vitesse = SubVectors(vehicule1->vitesse, compVitesse);
+//        vehicule2->vitesse = AddVectors(vehicule2->vitesse, compVitesse);
+        Vector2D tmpAcceleration = vehicule1->acceleration,
+                 tmpDirection = vehicule1->direction,
+                 tmpVitesse =vehicule1->vitesse;
+
+        vehicule1->acceleration = vehicule2->acceleration;
+//        vehicule1->direction = vehicule2->direction;
+        vehicule1->vitesse= vehicule2->vitesse;
+
+        vehicule1->direction.x *= vehicule2->direction.x;
+        vehicule1->direction.y *= vehicule2->direction.y;
+
+//        vehicule2->direction = tmpDirection;
+        vehicule2->acceleration = tmpAcceleration;
+
+        vehicule2->direction.x *= tmpDirection.x;
+        vehicule2->direction.y *= tmpDirection.y;
+
+        vehicule2->vitesse= tmpVitesse;
+        UpdateVehicule(vehicule1);
+        UpdateVehicule(vehicule2);
+    }
 }
 
 void CollisionVehiculeBallon(Ballon* ballon, Vehicule* vehicule){
-	if(TouchedVehiculeBallon(ballon, vehicule) == true){
+    if(TouchedVehiculeBallon(ballon, vehicule) == true){
         ballon->direction = MultVector(vehicule->direction, ballon->cercle->radius);
         ballon->acceleration = MultVector(ballon-> direction, 0.01);
     }
@@ -50,9 +70,9 @@ void CollisionVehiculeBallon(Ballon* ballon, Vehicule* vehicule){
 }
 
 void CollisionBallonTerrain(Ballon *ballon, Terrain * terrain){
-	if(TouchedBallonTerrain(ballon, terrain)){
-		//ballon->cercle->centre + ballon->cercle->radius
-	}
+    if(TouchedBallonTerrain(ballon, terrain)){
+        //ballon->cercle->centre + ballon->cercle->radius
+    }
 }
 /*if(xCercle + 0.5 + dx > 10. || xCercle - 0.5 + dx < -10.)
         dx *=-1;
@@ -77,24 +97,24 @@ bool CollisionCercleCercle(Cercle* c1, Cercle* c2) {
 }
 
 bool TouchedVehiculeVehicule(Vehicule* vehicule1, Vehicule* vehicule2){
-	Point2D posV = vehicule1->position;
-	float largeur = vehicule1->largeur;
-	float hauteur = vehicule1->hauteur;
-	float xMax = posV.x+largeur/2;
-	float xMin = posV.x-largeur/2;
-	float yMax = posV.y+hauteur/2;
-	float yMin = posV.y-hauteur/2;
+    Point2D posV = vehicule1->position;
+    float largeur = vehicule1->largeur;
+    float hauteur = vehicule1->hauteur;
+    float xMax = posV.x+largeur/2;
+    float xMin = posV.x-largeur/2;
+    float yMax = posV.y+hauteur/2;
+    float yMin = posV.y-hauteur/2;
 
-	Point2D pos11 = PointXY(xMin, yMax);
-	Point2D pos12 = PointXY(xMax, yMax);
-	Point2D pos13 = PointXY(xMax, yMin);
-	Point2D pos14 = PointXY(xMin, yMin);
+    Point2D pos11 = PointXY(xMin, yMax);
+    Point2D pos12 = PointXY(xMax, yMax);
+    Point2D pos13 = PointXY(xMax, yMin);
+    Point2D pos14 = PointXY(xMin, yMin);
 
-	if (IsTouchingVehicule(vehicule2, pos11) ||
-				IsTouchingVehicule(vehicule2, pos12) ||
-					IsTouchingVehicule(vehicule2, pos13) ||
-						IsTouchingVehicule(vehicule2, pos14))
-		return true;
+    if (IsTouchingVehicule(vehicule2, pos11) ||
+                IsTouchingVehicule(vehicule2, pos12) ||
+                    IsTouchingVehicule(vehicule2, pos13) ||
+                        IsTouchingVehicule(vehicule2, pos14))
+        return true;
 
     if (CollisionCercleCercle(vehicule1->cercle, vehicule2->cercle) ||
         CollisionCercleCercle(vehicule1->facticeCercle, vehicule2->cercle) ||
@@ -103,7 +123,7 @@ bool TouchedVehiculeVehicule(Vehicule* vehicule1, Vehicule* vehicule2){
         return true;
     }
 
-	return false;
+    return false;
 }
 
 
@@ -112,22 +132,22 @@ bool TouchedVehiculeBallon(Ballon* ballon, Vehicule* vehicule){
        CollisionCercleCercle(vehicule->facticeCercle, ballon->cercle)) {
         return true;
     }
-		
-	return false;
+
+    return false;
 }
 
 bool TouchedVehiculeCheckPoint(Vehicule* vehicule, Checkpoint* chkP){
-	if(CollisionCercleCercle(vehicule->cercle, chkP->cercle))
-		return true;
-	return false;
+    if(CollisionCercleCercle(vehicule->cercle, chkP->cercle))
+        return true;
+    return false;
 }
 
 
 bool TouchedBallonTerrain(Ballon *ballon, Terrain * terrain){
-	if(IsWall(terrain, ballon->cercle->centre) == true){
- 		return true;
+    if(IsWall(terrain, ballon->cercle->centre) == true){
+        return true;
 }
-	return false;
+    return false;
 }
 
 
